6547ec18af0b6d24050c9df18c3ce55d13e8c3e2
Ardra m s <ardrasivan2000@gmail.com>
2026-06-04 17:47:26 +0530

    feat:socket file added

---
+++ sockets/chatSocket.js
@@ -0,0 +1,37 @@
+const onlineUsers = {};
+
+const chatSocket = (io) => {
+    io.on("connection", (socket) => {
+        console.log("User connected:", socket.id);
+
+        socket.on("join", (userId) => {
+            onlineUsers[userId] = socket.id;
+            console.log("User joined:", userId);
+        });
+
+        socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+            const receiverSocketId = onlineUsers[receiverId];
+
+            if (receiverSocketId) {
+                io.to(receiverSocketId).emit("receiveMessage", {
+                    senderId,
+                    text
+                });
+            } else {
+                console.log("Receiver not online");
+            }
+        });
+
+        socket.on("disconnect", () => {
+            console.log("User disconnected:", socket.id);
+
+            for (let userId in onlineUsers) {
+                if (onlineUsers[userId] === socket.id) {
+                    delete onlineUsers[userId];
+                }
+            }
+        });
+    });
+};
+
+module.exports = chatSocket;
